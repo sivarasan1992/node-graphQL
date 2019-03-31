a9696deffcd2e39c867789ca6de5d043ba98ebe2
sivarasan1992 <sivarasan@technohive.tech>
2019-03-31 14:29:17 +0530

    added babel compiler and graphql types

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const express = require("express");
+import express from "express";
+import expressGraphQL from "express-graphql";
+import schema from "./schema/schema";
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(port, () => console.log(`serer is running on ${port}`));
